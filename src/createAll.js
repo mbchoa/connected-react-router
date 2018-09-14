a8b8548b4e54b0d6d8385928bd26c1ac5d2c14bf
Michael-Bryant Choa <mbchoa@gmail.com>
2018-09-14 13:51:17 -0700

    Expose routerReducer

--- src/createAll.js
+++ src/createAll.js
@@ -4,12 +4,16 @@ import createConnectRouter from './reducer'
 import routerMiddleware from './middleware'
 import createSelectors from './selectors'
 
-const createAll = structure => ({
-  ...actions,
-  ...createSelectors(structure),
-  ConnectedRouter: createConnectedRouter(structure),
-  connectRouter: createConnectRouter(structure),
-  routerMiddleware,
-})
+const createAll = structure => {
+  const { connectRouter, routerReducer } = createConnectRouter(structure)
+  return {
+    ...actions,
+    ...createSelectors(structure),
+    ConnectedRouter: createConnectedRouter(structure),
+    connectRouter,
+    routerMiddleware,
+    routerReducer,
+  }
+}
 
 export default createAll
